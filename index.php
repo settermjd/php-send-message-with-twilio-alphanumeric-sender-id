f7fc2d2fe4ba2d045e1ab69a498c7894ccf033fa
Matthew Setter <matthew@matthewsetter.com>
2023-03-07 16:44:35 +0100

    Format the file to meet the coding standard

--- index.php
+++ index.php
@@ -1,26 +1,32 @@
 <?php
 
+declare(strict_types=1);
+
 require_once './vendor/autoload.php';
 
 $dotenv = Dotenv\Dotenv::createImmutable(__DIR__);
 $dotenv->load();
-$dotenv->required(['TWILIO_ACCOUNT_SID', 'TWILIO_AUTH_TOKEN', 'MESSAGING_SERVICE_SID']);
+$dotenv->required([
+    'TWILIO_ACCOUNT_SID',
+    'TWILIO_AUTH_TOKEN',
+    'MESSAGING_SERVICE_SID',
+]);
 
 use Twilio\Rest\Client;
 
-$sid = $_ENV["TWILIO_ACCOUNT_SID"];
-$token = $_ENV["TWILIO_AUTH_TOKEN"];
-$messagingServiceSID = $_ENV["MESSAGING_SERVICE_SID"];
-$twilio = new Client($sid, $token);
+$twilio = new Client(
+    $_ENV["TWILIO_ACCOUNT_SID"],
+    $_ENV["TWILIO_AUTH_TOKEN"]
+);
 
 $message = $twilio
     ->messages
     ->create(
         "+4917643971069",
         [
-            "body" => "This is a test message",
-            "messagingServiceSid" => $messagingServiceSID
+            "body"                => "This is a test message",
+            "messagingServiceSid" => $_ENV["MESSAGING_SERVICE_SID"],
         ]
     );
 
-print($message->sid);
+print $message->sid;
